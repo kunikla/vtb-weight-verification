0fbcfd238fe49473b19ef5740d859330f0e0bf63
Christina Hamilton <kunikla.vt@gmail.com>
2018-01-25 07:43:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# VTB Weight Verification App
+
+Version 1.0.0 - 1/25/18
+* initial commit
+
+# TODO
+##UX
+* change colors in form to match logo
+
+## Functionality
+* add button in header to reset all forms
+* add button in .data-entry to reset deviation, display in .dev-pct in .results
+* add validation routines for input fields
+* add on-click validation routines for buttons
+
---
+++ js/main.js
@@ -0,0 +1,74 @@
+/*
+ * Validation Routines
+ */
+
+/*
+ * PO must be 5 digits
+ */
+function checkPO () {
+  
+}
+
+/*
+ * PAJ code must be PAJ/PJJ followed by 5 digits
+ */
+function checkPAJ () {
+  
+}
+
+/*
+ * Full Box Weight must be number with 2 decimal places
+ * Might allow lbs/oz at some point
+ */
+function checkFullBoxWeight () {
+  
+}
+
+/*
+ * Empty Box Weight must be number with 2 decimal places
+ * Might allow lbs/oz at some point
+ */
+function checkEmptyBoxWeight () {
+  
+}
+
+/*
+ * productCount must be integer >0
+ */
+function checkProductCount () {
+  
+}
+
+/*
+ * Product Weight must be number with 2 decimal places
+ * Might allow lbs/oz at some point
+ */
+function checkProductCount () {
+
+}
+
+function nextOp1 () {
+  localStorage.setItem('VTBCountVerification', JSON.stringify(formObject));
+}
+
+/*
+ * Other Event Handlers
+ */
+
+/*
+ * Mainline code
+ */
+
+// declare variables
+var formObject; {
+  "purchaseOrder", "";
+  "pajCode", "";
+  "fullBoxWeight", 0;
+  "emptyBoxWeight", 0;
+  "productCount", 0;
+}
+// initialize validation routines
+
+
+// 
+
